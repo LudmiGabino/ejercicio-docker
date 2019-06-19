3a9a6ece2c982a73cc85bf6dcc5c194603c00c98
Ludmila Gabino <jolumi7@gmail.com>
2019-06-19 19:54:28 -0300

    ejercicio docker

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/ejemplo', function(req, res){
+  res.send('ejemplo con docker')
+})
+
+app.listen(8080, '0.0.0.0');
+module.exports = app;
